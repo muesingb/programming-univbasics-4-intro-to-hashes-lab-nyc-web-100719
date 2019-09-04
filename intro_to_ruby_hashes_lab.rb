81e2dc0c3112d062dd8eda1ad1e6443d7c4aa8fc
Barbara Muesing <muesingb@gmail.com>
2019-09-04 20:21:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def id_generator
 end
 # return a hash that includes the key and value parameters passed into this method
 def my_hash_creator(key, value)
-  {:key => value}
+  {key => value}
 end
 # return the correct value using the hash and key parameters
 def read_from_hash(hash, key)
