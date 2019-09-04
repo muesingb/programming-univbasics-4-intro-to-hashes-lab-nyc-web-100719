76bd60aec490f461cb88d45ed2f3fa8b334282c8
Barbara Muesing <muesingb@gmail.com>
2019-09-04 20:18:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 # creates and returns a new, empty hash.
 def new_hash
-  hash.new()
+  return hash.new()
 end
 # return a valid hash with any key/value pair of your choice
 def my_hash
@@ -26,8 +26,8 @@ end
 # if the provided key is not present in the hash, add it and assign it to the value of 1
 # if the provided key is present, increment its value by 1
 def update_counting_hash(hash, key)
-  if hash[key]
-    hash[key] += 1
-  else
-    hash[key] = 1
+#  if hash[key]
+#    hash[key] += 1
+#  else
+#    hash[key] = 1
 end
